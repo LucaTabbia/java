57002ee73d5a25b78a739455bb049457b7aca15b
Tabbia Luca <luca.tabbia97@gmail.com>
2021-02-19 11:12:41 +0100

    add Studenti

---
+++ Filippo/Lezione 3/src/com/company/Compito.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Compito {
+    protected int id;
+    private static int nextId= 0;
+
+    public Compito(){
+        this.id=nextId;
+        nextId++;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    @Override
+    public String toString() {
+        return "Compito{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ Filippo/Lezione 3/src/com/company/Studente.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Studente extends Persona {
+    private int id;
+
+    public Studente(String nome, String cognome, Sesso sesso, int id) {
+        super(nome, cognome, sesso);
+        this.id=id;
+    }
+
+    @Override
+    public String toString() {
+        return "Studente: {" + "id=" + id+ getDataString()+ "}";
+    }
+}
